8bbfb5eca22ac27c15b54cfa4d8f7719a447f9d5
Justin <justin.leung@strath.ac.uk>
2020-03-12 17:26:07 +0000

    testing if gitlab connected properly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 
+//testing if gitlab connected properly
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Player p = new Player("Bob", 0, 0, 0, 0);
